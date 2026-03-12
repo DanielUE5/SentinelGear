383dfbea68ef8eb32ad5d84eba14ae284cb3e1ef
Daniel <ivanovdaniel51@gmail.com>
2026-03-13 00:06:12 +0200

    Cart functionalities added. New helper SessionExtensions added. New view Cart/Index.cshtml added. Products' views adjusted. Program.cs improved.

---
+++ SentinelGear/ViewModels/CartItemViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace SentinelGear.ViewModels
+{
+    public class CartItemViewModel
+    {
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; } = null!;
+
+        public string? ImageUrl { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int StockQuantity { get; set; }
+
+        public decimal TotalPrice => UnitPrice * Quantity;
+    }
+}
