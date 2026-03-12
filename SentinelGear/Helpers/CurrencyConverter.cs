18cbacd0b2096f5d5536bd5b02f1fa77d286cbee
Daniel <ivanovdaniel51@gmail.com>
2026-03-12 22:10:47 +0200

    To display the prices in Bulgaria currently, it is indeed important to have the prices in euro and leva displayed

---
+++ SentinelGear/Helpers/CurrencyConverter.cs
@@ -0,0 +1,12 @@
+﻿namespace SentinelGear.Helpers
+{
+    public static class CurrencyConverter
+    {
+        private const decimal BgnToEurRate = 1.95583m;
+
+        public static decimal ConvertBgnToEur(decimal priceInBgn)
+        {
+            return Math.Round(priceInBgn / BgnToEurRate, 2);
+        }
+    }
+}
